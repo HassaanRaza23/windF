0a51834d1e301e538061b9253fb576ec71df2ac1
Safa-UI <95444315+Safa-UI@users.noreply.github.com>
2021-12-03 08:16:15 +0000

    line 6

--- src/class1.java
+++ src/class1.java
@@ -1,8 +1,11 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
 public class class1 {
     public static void main(String[] args) {
         System.out.println("say hello");
         System.out.println("updated");
         System.out.println("version updated");
         System.out.println("how about printing another sentence?");
+        System.out.println("editing now");
     }
 }
